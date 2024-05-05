7f3e66e11b444407ba9919e841c44df280176818
Barbora Fabianova <BARBORKA.FABIANOVA@GMAIL.COM>
2024-05-05 16:21:51 +0000

    preparation for deployment

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -13,7 +13,16 @@ if os.path.exists("env.py"):
 # Now we can create an instance of the imported Flask() class, and that will be stored in a variable called app, which takes the default Flask name module. Then, we need to specify two app configuration variables, and these will both come from our environment variables. app.config SECRET_KEY and app.config SQLALCHEMY_DATABASE_URI, both wrapped in square brackets and quotes. Each of these will be set to get their respective environment variable, which is SECRET_KEY, and the short and sweet DB_URL for the database location which we'll set up later.
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "True":
+     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri
+
+#app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 
 #Then, we need to create an instance of the imported SQLAlchemy() class, which will be
